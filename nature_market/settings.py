fb17912d3bf4572eb1615d565bc15fe7b728a187
Anthonyhambrugi <anthonyhambrugi@gmail.com>
2026-01-14 01:23:05 -0300

    Favicon

--- nature_market/settings.py
+++ nature_market/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
