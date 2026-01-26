283c6ec1894ee212a2397088f256b0a3c9cfd87f
Anthonyhambrugi <anthonyhambrugi@gmail.com>
2026-01-25 22:26:59 -0300

    pro render calar a boca 4

--- naturemarket/settings.py
+++ naturemarket/settings.py
@@ -123,13 +123,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
