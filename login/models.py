f935049cf124ab8d45bd22cedf9480047edfbdf4
Anthonyhambrugi <anthonyhambrugi@gmail.com>
2026-01-26 15:29:15 -0300

    sadnweddnn

--- login/models.py
+++ login/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class LoginModel (models.Model):
-    username = models.CharField()
+    username = models.CharField(max_length=20)
     password = models.CharField(max_length=15)
 
     def __str__(self):
-        return self.nome
+        return self.username
